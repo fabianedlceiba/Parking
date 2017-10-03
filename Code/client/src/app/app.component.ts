4ab61b65e4e572e2a843a789773d4e303da518bc
CEIBA\fabian.diaz <fabian.diaz@ceiba.com.co>
2017-10-03 07:39:18 -0500

    Init Commit

--- Code/client/src/app/app.component.ts
+++ Code/client/src/app/app.component.ts
@@ -16,15 +16,4 @@ export class AppComponent implements OnInit {
       $.material.options.autofill = true;
       $.material.init();
   }
-
-    isMaps(path){
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      titlee = titlee.slice( 1 );
-      if(path == titlee){
-        return false;
-      }
-      else {
-        return true;
-      }
-    }
 }
--- Code/client/src/app/app.module.ts
+++ Code/client/src/app/app.module.ts
@@ -14,7 +14,6 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { TableListComponent } from './table-list/table-list.component';
 import { TypographyComponent } from './typography/typography.component';
 import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { UpgradeComponent } from './upgrade/upgrade.component';
 
@@ -26,7 +25,6 @@ import { UpgradeComponent } from './upgrade/upgrade.component';
     TableListComponent,
     TypographyComponent,
     IconsComponent,
-    MapsComponent,
     NotificationsComponent,
     UpgradeComponent,
 
@@ -36,7 +34,7 @@ import { UpgradeComponent } from './upgrade/upgrade.component';
     FormsModule,
     HttpModule,
     ComponentsModule,
-RouterModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
