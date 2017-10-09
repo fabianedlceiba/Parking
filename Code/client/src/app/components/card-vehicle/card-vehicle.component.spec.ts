ca2df357c685f98414fab099400afc66a34816aa
Fabian Diaz <fabian.diaz@ceiba.com.co>
2017-10-09 11:27:45 -0500

    Test of the component card-vehicle.

--- Code/client/src/app/components/card-vehicle/card-vehicle.component.spec.ts
+++ Code/client/src/app/components/card-vehicle/card-vehicle.component.spec.ts
@@ -1,28 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from "@angular/platform-browser";
 
 import { CardVehicleComponent } from './card-vehicle.component';
+import { CarPark } from './car-park';
+import { DebugElement } from '@angular/core';
 
-describe('FooterComponent', () => {
+describe('CardVehicleComponent', () => {
   let component: CardVehicleComponent;
   let fixture: ComponentFixture<CardVehicleComponent>;
 
-  /*beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CardVehicleComponent ]
-    })
-    .compileComponents();
-  }));*/
-
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CardVehicleComponent]
-    }).compileComponents();
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(CardVehicleComponent);
     component = fixture.componentInstance;
-    /*fixture.detectChanges();*/
+    //fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create 2', () => {
+    
+    const carPark: CarPark = new CarPark();
+    carPark.entryDate = '01-10-2017 12:00 P.M'
+    component.carPark = carPark;
+    fixture.detectChanges();
+
+    const entryDateElement: any = fixture.debugElement.query(By.css('.stats')).nativeElement;
+
+    expect(entryDateElement.textContent).toContain(carPark.entryDate);
+  });
+
 });
