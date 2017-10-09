cd07985f4da11d7d25bca71d077fb15755d250f4
Fabian Diaz <fabian.diaz@ceiba.com.co>
2017-10-09 09:30:40 -0500

    gui

--- Code/api/src/main/java/co/com/ceiba/parking/helpers/CustomLocalDateTimeSerializer.java
+++ Code/api/src/main/java/co/com/ceiba/parking/helpers/CustomLocalDateTimeSerializer.java
@@ -1,28 +1,29 @@
 package co.com.ceiba.parking.helpers;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
 public class CustomLocalDateTimeSerializer extends StdSerializer<LocalDateTime> {
 
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
+  private static final long serialVersionUID = 5334509585679436656L;
 
-    public CustomLocalDateTimeSerializer() {
-        this(null);
-    }
+  private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
 
-    public CustomLocalDateTimeSerializer(Class<LocalDateTime> t) {
-        super(t);
-    }
+  public CustomLocalDateTimeSerializer() {
+    this(null);
+  }
 
-    @Override
-    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider arg2)
-            throws IOException, JsonProcessingException {
-        gen.writeString(formatter.format(value));
-    }
+  public CustomLocalDateTimeSerializer(Class<LocalDateTime> t) {
+    super(t);
+  }
+
+  @Override
+  public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider arg2) throws IOException {
+    gen.writeString(formatter.format(value));
+  }
 }
